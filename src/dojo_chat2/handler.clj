b05ca1fde9dce0339b9733f62007ae1a25763b03
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-03-09 21:28:23 +0000

    add more

--- src/dojo_chat2/handler.clj
+++ src/dojo_chat2/handler.clj
@@ -10,23 +10,26 @@
   (println (format "user = %s ,users = %s" user @users))
   (if-not (contains? @users user)
     (do
-      (reset! users (assoc @users user []))
+      (reset! users (assoc @users user (atom [])))
       {:status 201 :body (format "Created user %s" user) :headers {"Content-Type" "text/plain"}})
     {:status 400 :body "User already existing"}))
 
 (defn get-messages
   [user]
-  (str (@users user)))
+  (str @(@users user)))
 
 (defn send-message
-  [from to]
-  )
+  [from to message]
+  (swap! (@users to) (fn [x] (conj x message)))
+  {:status 201 :body (format "Sent message to user %s" to)})
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
   (POST "/register" kwargs (register (:user (:params kwargs))))
+  (POST "/send" kwargs (send-message (:to (:params kwargs)) (:message (:params kwargs))))
   (GET "/messages" [user] (get-messages user))
   (route/not-found "Not Found"))
 
 (def app
   (wrap-defaults app-routes api-defaults))
+) 
