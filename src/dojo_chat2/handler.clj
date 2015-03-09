fd364079ab921581e4e10410568fd104d61d85f1
Andrea Crotti <andrea.crotti.0@gmail.com>
2015-03-09 20:10:31 +0000

    register function

--- src/dojo_chat2/handler.clj
+++ src/dojo_chat2/handler.clj
@@ -5,7 +5,15 @@
 
 (defroutes app-routes
   (GET "/" [] "Hello World")
+  (POST "/" [user] (register user))
   (route/not-found "Not Found"))
+
+(def users (atom {}))
+
+(defn register
+  [user]
+  (if-not (contains? @users user)
+    (reset! users (assoc @users user []))))
 
 (def app
   (wrap-defaults app-routes site-defaults))
