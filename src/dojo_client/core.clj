a55d3d93e8d5bd531a3289b9025c9f25bab7f950
Chris Howe-Jones <chris.howejones@gmail.com>
2015-03-09 22:14:10 +0000

    anged client to try and use do-loop. Not working yet.

--- src/dojo_client/core.clj
+++ src/dojo_client/core.clj
@@ -2,17 +2,34 @@
   (:require [org.httpkit.client :as client]
             [clojure.core.async :as async]))
 
-(def response (client/get "http://localhost:3000"))
+
+(defn response-messages [user] (client/get (str "http://localhost:3000/messages?user=" user) ))
 
 (def c (async/chan))
 
-(defn poll-server []
+(defn poll-server [user]
   (async/go
-    (async/>! c (:body @response))))
+    (while true
+      (async/>! c (:body @(response-messages user)))
+      (Thread/sleep 2000))))
 
 
 (defn print-channel []
-  (async/go
-    (while true
-     (println "Result = "
-              (async/<! c)))))
+  (async/go-loop []
+    (println "Result = "
+             (async/<! c))))
+
+
+(defn register-user [name]
+  (let [options {:form-params {:user name}}
+        {:keys [status error]} @(client/post "http://localhost:3000/register" options)]
+    (if error
+      (println "Failed, exception is " error)
+      (println "Async HTTP POST: " status))))
+
+(defn send-message [name message]
+  (let [options {:form-params {:to name :message message}}
+        {:keys [status error]} @(client/post "http://localhost:3000/send" options)]
+    (if error
+      (println "Failed, exception is " error)
+      (println "Async HTTP POST: " status))))
